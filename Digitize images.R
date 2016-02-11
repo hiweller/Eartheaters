45412c4868345ac1e65a21ed8a666cc3119d19e3
Hannah Weller <hiweller@uchicago.edu>
2016-02-11 09:52:08 -0600

    added digitize/read images scripts

--- Digitize images.R
+++ Digitize images.R
@@ -1,10 +1,11 @@
+setwd('/Users/hannah/Dropbox/Westneat Lab/Eartheater Project/Data/Videos/')
 library(StereoMorph)
 
 run <- function(){
 
 	digitizeImages(
-		image.file='Images', 
-		shapes.file='Shapes', 
+  		image.file='02-06-16/Intervals/S_SD3_01_5FrameIntervals/', 
+		shapes.file='02-06-16/Shapes/S_SD3_01_5FrameShapes/', 
 		landmarks.ref='landmarks.txt', 
 		curves.ref='curves.txt'
 	)
